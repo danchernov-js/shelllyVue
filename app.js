60725578d263a874613d23c7e00620332593ee54
Daniil Chernov <chernov93d@gmail.com>
2021-03-04 23:33:08 +0300

    куча переделок проекта на вью

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var app = new Vue({
   el: "#app",
+  components: {
+    "image-preview": imageComponent,
+  },
   data: {
     dragging: false,
     imageArr: [],
@@ -47,6 +50,56 @@ var app = new Vue({
       this.pushCanvasData(resultCanvasData, this.$data.imageDownArr);
 
       this.$data.i++;
+    },
+    restoreDownloadButton() {
+      const downloadProgress = document.getElementById("download");
+      downloadProgress.disabled = false;
+      downloadProgress.innerHTML = "Загрузить ещё";
+    },
+    downloadResult() {
+      const downloadProgress = document.getElementById("download");
+      downloadProgress.disabled = true;
+      downloadProgress.innerHTML = "Загрузка...";
+
+      if (this.$data.imageArr.length == 1) {
+        const item = this.$data.imageArr[0];
+        const gCanvas = document.getElementById(item.cid);
+        gCanvas.toBlob((blob) => {
+          const a = document.createElement("a");
+          document.body.append(a);
+          a.download =
+            "MacPic-" + item.fname.substring(0, item.fname.lastIndexOf("."));
+          a.href = URL.createObjectURL(blob);
+          a.click();
+          a.remove();
+          this.restoreDownloadButton();
+        });
+      } else {
+        var zip = new JSZip();
+        var fileTasks = [];
+        this.$data.imageArr.forEach((item) => {
+          let gCanvas = document.getElementById(item.cid);
+          let task = new Promise((resolve, reject) => {
+            gCanvas.toBlob((blob) => {
+              let fileName = item.fname.substring(
+                0,
+                item.fname.lastIndexOf(".")
+              );
+              zip.file(`macshell-${fileName}.png`, blob);
+              resolve();
+            });
+          });
+          fileTasks.push(task);
+        });
+
+        Promise.all(fileTasks).then(() => {
+          zip.generateAsync({ type: "blob" }).then((content) => {
+            const timestamp = Date.now().toString();
+            saveAs(content, `macshell-${timestamp}.zip`);
+            this.restoreDownloadButton();
+          });
+        });
+      }
     },
     getCanvasData(canvasId, fileLink, fileName) {
       return {
--- components/image.js
+++ components/image.js
@@ -1,10 +1,243 @@
-var imageComponent = {
-  data: {
-    item: {}
+const imageComponent = {
+  props: ["itemdata"],
+  data() {
+    return {
+      item: this.itemdata,
+    };
   },
-  created() {
+  mounted() {
+    let gCanvas = document.getElementById(this.$data.item.cid);
     this.$data.item.img = new Image();
     this.$data.item.img.src = this.$data.item.flink;
-    this.$data.item.img.onload = () => this.drawImage(gCanvas, item);
-  }
-}
+    this.$data.item.img.onload = () => this.drawImage(gCanvas, this.$data.item);
+  },
+  template:
+    '<div class="card">' +
+    '<p class="image-name" v-bind:id="item.cid + \'-wrap\'"></p>' +
+    '<canvas v-bind:id="item.cid"></canvas>' +
+    "</div>",
+  methods: {
+    roundRect(ctx, x, y, width, height, radius, fill, stroke) {
+      if (typeof stroke === "undefined") {
+        stroke = true;
+      }
+      if (typeof radius === "undefined") {
+        radius = 5;
+      }
+      if (typeof radius === "number") {
+        radius = { tl: radius, tr: radius, br: radius, bl: radius };
+      } else {
+        var defaultRadius = { tl: 0, tr: 0, br: 0, bl: 0 };
+        for (var side in defaultRadius) {
+          radius[side] = radius[side] || defaultRadius[side];
+        }
+      }
+      ctx.beginPath();
+      ctx.moveTo(x + radius.tl, y);
+      ctx.lineTo(x + width - radius.tr, y);
+      ctx.quadraticCurveTo(x + width, y, x + width, y + radius.tr);
+      ctx.lineTo(x + width, y + height - radius.br);
+      ctx.quadraticCurveTo(
+        x + width,
+        y + height,
+        x + width - radius.br,
+        y + height
+      );
+      ctx.lineTo(x + radius.bl, y + height);
+      ctx.quadraticCurveTo(x, y + height, x, y + height - radius.bl);
+      ctx.lineTo(x, y + radius.tl);
+      ctx.quadraticCurveTo(x, y, x + radius.tl, y);
+      ctx.closePath();
+      if (fill) {
+        ctx.fill();
+      }
+      if (stroke) {
+        ctx.stroke();
+      }
+    },
+    drawImage(gCanvas, item) {
+      const gCtx = gCanvas.getContext("2d");
+
+      //Variables
+      var x = 20; //для ширины
+      var y = 10; //для высоты
+      var barH = 40; //для высоты хэдера
+      var borderR = 10; //для скругления
+      var circleOffsetX = 14; //для смещения круга по оси х
+      var circleR = 5; //для радиус кружочка мака
+
+      //Canvas size
+      gCanvas.width = item.img.width + x * 2;
+      gCanvas.height = item.img.height + y * 4 + barH;
+
+      //Browser window shadow;
+      gCtx.globalAlpha = 0.2; //прозрачноть 20%
+      gCtx.shadowColor = "black";
+      gCtx.shadowOffsetY = 10; //отображение вниз на 10
+      gCtx.shadowBlur = 15; //размытие тени
+      gCtx.fillStyle = "rgb(201,201,201)"; //чекнуть, можно убрать
+      this.roundRect(
+        gCtx,
+        x,
+        y,
+        item.img.width,
+        item.img.height + barH,
+        borderR,
+        true,
+        false
+      ); //скругленные углы тени
+
+      //Browser window; удалить
+      gCtx.globalAlpha = 1;
+      gCtx.shadowOffsetY = 0; //чекнуть, можно ли убрать
+      gCtx.shadowBlur = 0;
+      gCtx.fillStyle = "rgb(201,201,201)"; //чекнуть можно ли убрать
+      this.roundRect(
+        //чекнуть можно л убрать
+        gCtx,
+        x,
+        y,
+        item.img.width,
+        item.img.height + barH,
+        borderR,
+        true,
+        false
+      );
+
+      //Browser bar
+      gCtx.shadowOffsetY = 0;
+      gCtx.shadowBlur = 0;
+      gCtx.fillStyle = "rgb(255,255,255)";
+      this.roundRect(
+        gCtx,
+        x,
+        y,
+        item.img.width,
+        barH,
+        { tl: borderR, tr: borderR },
+        true,
+        false
+      );
+
+      //Red circle ободок
+      gCtx.fillStyle = "rgb(223,72,69)";
+      this.roundRect(
+        gCtx,
+        x + circleOffsetX + 20 * 0,
+        y + circleOffsetX,
+        12,
+        12,
+        circleR + 1,
+        true,
+        false
+      );
+      //красное тело
+      gCtx.fillStyle = "rgb(252,97,93)";
+      this.roundRect(
+        gCtx,
+        x + circleOffsetX + 20 * 0 + 1,
+        y + circleOffsetX + 1,
+        12 - 2,
+        12 - 2,
+        circleR,
+        true,
+        false
+      );
+
+      //Yellow circle
+      gCtx.fillStyle = "rgb(222,160,52)";
+      this.roundRect(
+        gCtx,
+        x + circleOffsetX + 20 * 1,
+        y + circleOffsetX,
+        12,
+        12,
+        circleR + 1,
+        true,
+        false
+      );
+
+      gCtx.fillStyle = "rgb(253,189,65)";
+      this.roundRect(
+        gCtx,
+        x + circleOffsetX + 20 * 1 + 1,
+        y + circleOffsetX + 1,
+        12 - 2,
+        12 - 2,
+        circleR,
+        true,
+        false
+      );
+
+      //Green circle
+      gCtx.fillStyle = "rgb(40,171,53)";
+      this.roundRect(
+        gCtx,
+        x + circleOffsetX + 20 * 2,
+        y + circleOffsetX,
+        12,
+        12,
+        circleR + 1,
+        true,
+        false
+      );
+
+      gCtx.fillStyle = "rgb(52,200,74)";
+      this.roundRect(
+        gCtx,
+        x + circleOffsetX + 20 * 2 + 1,
+        y + circleOffsetX + 1,
+        12 - 2,
+        12 - 2,
+        circleR,
+        true,
+        false
+      );
+
+      //Browser search bar
+      gCtx.fillStyle = "rgba(230,230,230,0.64)";
+      this.roundRect(
+        gCtx,
+        x + 77,
+        y + 7,
+        gCanvas.width - x * 2 - 77 * 2,
+        24,
+        circleR + 1,
+        true,
+        false
+      );
+
+      //Browser address
+      gCtx.fillStyle = "rgb(110,110,110)";
+      gCtx.strokeStyle = "rgba(0,0,0,0)";
+      gCtx.font = "14px Arial";
+      gCtx.fillText("benzodoctor.ru", item.img.width / 2 - 42, barH - 6);
+
+      //Image mask
+      gCtx.beginPath();
+      this.roundRect(
+        gCtx,
+        x,
+        y + barH,
+        item.img.width,
+        item.img.height,
+        { bl: borderR, br: borderR },
+        true,
+        false
+      );
+      gCtx.clip();
+      gCtx.drawImage(item.img, x, y + barH);
+      gCtx.strokeStyle = "rgb(201,201,201)"; //цвет обводки
+      this.roundRect(
+        gCtx,
+        x,
+        y + barH - 1,
+        item.img.width,
+        item.img.height + 1,
+        { bl: borderR, br: borderR },
+        false,
+        true
+      );
+    },
+  },
+};
